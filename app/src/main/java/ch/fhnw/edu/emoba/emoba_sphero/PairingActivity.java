eefcbb2da92aa3bdcd86658f6e313fa5c6c8200b
Janis <Fallyx@users.noreply.github.com>
2018-03-30 15:46:55 +0200

    Initial commit

---
+++ app/src/main/java/ch/fhnw/edu/emoba/emoba_sphero/PairingActivity.java
@@ -0,0 +1,15 @@
+package ch.fhnw.edu.emoba.emoba_sphero;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PairingActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pairing);
+    }
+}
