dab9ed224a6553a24fe87643c1af553d735f1922
Fabio Strappazzon <fabio.strappazzon@students.fhnw.ch>
2018-04-17 11:10:03 +0200

    add toggle button for sensor

--- app/src/main/java/ch/fhnw/edu/emoba/emoba_sphero/SensorFragment.java
+++ app/src/main/java/ch/fhnw/edu/emoba/emoba_sphero/SensorFragment.java
@@ -14,16 +14,23 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SensorFragment extends Fragment implements SensorEventListener
-
 {
     SpheroWrapper wrapper;
     private SensorManager sensorManager;
+    private boolean sensorOn = false;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_sensor, container, false);
+        view.findViewById(R.id.sensorToggle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setSensor(!sensorOn);
+            }
+        });
         sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
         return view;
     }
@@ -31,15 +38,12 @@ public class SensorFragment extends Fragment implements SensorEventListener
     @Override
     public void onResume(){
         super.onResume();
-        sensorManager.registerListener(this,
-                sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR),
-                SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     public void onPause(){
         super.onPause();
-        sensorManager.unregisterListener(this);
+        setSensor(false);
     }
 
 
@@ -64,6 +68,20 @@ public class SensorFragment extends Fragment implements SensorEventListener
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    private void setSensor(boolean state){
+        if(state){
+            sensorManager.unregisterListener(this);
+            ((TextView)getActivity().findViewById(R.id.textViewSensor)).setTextColor(0xff00ff00);
+        }else{
+            sensorManager.registerListener(this,
+                    sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR),
+                    SensorManager.SENSOR_DELAY_NORMAL);
+            ((TextView)getActivity().findViewById(R.id.textViewSensor)).setTextColor(0xffff0000);
+        }
+        sensorOn = state;
 
     }
 }
